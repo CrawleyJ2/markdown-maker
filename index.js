8f26fbe134607ed2d30d2fce03dd86b0b8dfbadd
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-06-11 18:40:03 -0400

    adding placeholder questions

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt ([
+        {
+            // title of project
+        },
+        {
+            // description of what, why, how
+        },
+        {
+            //installation steps
+        },
+        {
+            // usage w/ screenshots
+        },
+        {
+            // credits of collaborators, third-party assets, tutorials used
+        },
+        {
+            // license for others to know whay they can and cannot do with your project
+        },
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
