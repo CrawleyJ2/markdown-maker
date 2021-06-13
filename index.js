4a8ce1d1a6975cebd9c8f196a98f38885c059f38
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-06-13 17:19:32 -0400

    updated questions array and initial functionality

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const writeToFile = require ('./utils/generateMarkdown');
+const inquirer = require ('inquirer');
+const fs = require ('fs');
+const createMD = require ('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt ([
         {
             type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        },
+        {
+            type: 'input',
             name: 'title',
-            message: 'What is the name of your Project? (Required)',
-            //validate: ''
+            message: 'What is the title of your Project?',
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Please provide a description of the project: (Required)',
-            //validate: ''
+            message: 'Please provide a description of the project:',
         },
         {
             type: 'input',
-            name: 'process',
-            message: 'What steps are required to install your project? (Required)',
-            //validate: ''
+            name: 'installation',
+            message: 'What steps are required to install your project?',
         },
         {
             type: 'input',
@@ -30,16 +38,46 @@ const questions = () => {
 
         },
         {
-            // credits of collaborators, third-party assets, tutorials used
+            type: 'input',
+            name: 'contribution',
+            message: 'What should users know about contributing to your project?'
         },
         {
-            // license for others to know whay they can and cannot do with your project
+            type: 'input',
+            name: 'testing',
+            message: 'How would the user test this project?'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What license should your project have?',
+            choices: [
+                "MIT License",
+                "Mozilla Public License 2.0",
+                "Boost Software License 1.0",
+                "The Unlicense",
+                "Apache License 2.0"
+            ]
         },
     ]);
 };
 
 // TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
+const writeToFile = (fileContent) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/README.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'README file created in dist folder!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
 //function init() {}
